1a76d172ae6c19cce8fd0328443ced7cd360ac32
ROMA <roman247.rg@gmail.com>
2020-08-11 19:03:02 -0500

    react-curso

---
+++ precache-manifest.4b732499a76338dc6ca353faafef6eda.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53412df9150d82d546c7e6eab1ba50db",
+    "url": "/curso-react/index.html"
+  },
+  {
+    "revision": "d881f118a2adf7277ef8",
+    "url": "/curso-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "080f9b9d9a3e7cb38b4b",
+    "url": "/curso-react/static/css/main.1618a47b.chunk.css"
+  },
+  {
+    "revision": "d881f118a2adf7277ef8",
+    "url": "/curso-react/static/js/2.0df1ce2b.chunk.js"
+  },
+  {
+    "revision": "9d8639c7be71ec07fbbad9e6c05d0b23",
+    "url": "/curso-react/static/js/2.0df1ce2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080f9b9d9a3e7cb38b4b",
+    "url": "/curso-react/static/js/main.9c5a5fa7.chunk.js"
+  },
+  {
+    "revision": "6b4ce4619e8b5c137622",
+    "url": "/curso-react/static/js/runtime-main.85636a52.js"
+  },
+  {
+    "revision": "75f17d14e706a984e5edc5a235c1fbab",
+    "url": "/curso-react/static/media/angular.75f17d14.png"
+  },
+  {
+    "revision": "941c57307970d61edd050e7d181a3d23",
+    "url": "/curso-react/static/media/comunidad1.941c5730.svg"
+  },
+  {
+    "revision": "1291b69617e0772f1867c02136d0557a",
+    "url": "/curso-react/static/media/undraw_code_review_l1q9.1291b696.svg"
+  },
+  {
+    "revision": "3d0c546abf5839b05984b4081d6e9153",
+    "url": "/curso-react/static/media/undraw_completing_6bhr.3d0c546a.svg"
+  },
+  {
+    "revision": "7b7361e6678d2dcb7a90f2afd1fbf92a",
+    "url": "/curso-react/static/media/undraw_mobile_login_ikmv.7b7361e6.svg"
+  },
+  {
+    "revision": "798f45e77da034e1340cc275927f25c2",
+    "url": "/curso-react/static/media/undraw_press_play_bx2d.798f45e7.svg"
+  }
+]);
